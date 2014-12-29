11cba71744dfc10d48a723d8544bedf9a5eb3397
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>
    
    AOKP animations: Add an entry for TRANSIT_TASK_OPEN_BEHIND [1/2]
    
    Change-Id: I306c932c79d32e727f15fe1645ffa723d3169bec
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>
    
    Fix animation duration for custom animations
    
    Add missing helper

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3712,6 +3712,25 @@ public final class Settings {
          */
         public static final String OPA_ANIM_DURATION_Y = "opa_anim_duration_y";
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * Pixel Anim Duration
          * @hide
