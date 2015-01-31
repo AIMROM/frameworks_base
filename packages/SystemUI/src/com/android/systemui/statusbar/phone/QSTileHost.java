57ec8378c906c72da8d1707ae748d2f6da30c478
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-31 22:45:50 +0000

    QS: add Sync tile (1/2)
    Change-Id: I0c0bb51e736a01661566f1118f955e32eba96063

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -63,6 +63,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.ImageTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.MusicTile;
@@ -475,6 +476,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 		else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
 		else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
 		else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
+		else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.equals(mImageTileSpec)) {
             if (isImageTileInstalled()) {
                 return new ImageTile(this);
