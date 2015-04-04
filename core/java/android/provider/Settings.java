36026aaebace10439a243059163fd1d4a71defb8
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    Add membar to recents options [1/2]
    https://github.com/BenzoRoms/frameworks_base/commit/98de755cb5cb56b1778fd7fd38a9d36e90b2ade5
    https://github.com/BenzoRoms/frameworks_base/commit/0a454d19e453b7294c6df54cbb80b217405a2798
    
    thanks to varund7726 for the spacing
    
    Change-Id: Id1e799729543e385767ec8b68b126a20c4874baf
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/aim_dimens.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4000,6 +4000,12 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
 
         /**
+         * show the membar in recents
+         *  @hide
+         */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
