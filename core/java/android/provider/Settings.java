b72ed1e54e24a466786da6a51113fdc3a4b6db5f
BeansTown106 <nbenis106@gmail.com>
2016-09-24 19:58:49 -0700

    Add toggle for fancy QS animation [1/2]
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4184,6 +4184,12 @@ public final class Settings {
         public static final String BLUR_MIXED_COLOR_PREFERENCE_KEY = "blur_mixed_color_pref";
 
         /**
+         * Quick Settings nougat animation
+         * @hide
+         */
+        public static final String QS_FANCY_ANIMATION = "qs_fancy_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
