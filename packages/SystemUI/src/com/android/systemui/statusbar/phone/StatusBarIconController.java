1b396c719d0b4694a796966c1371d13ce2d69f7a
beanstown106 <nbenis106@gmail.com>
2016-10-20 19:40:56 +0530

    Carrier Label: Extend it to Both Statusbar & Keyguard [1/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -340,13 +340,29 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
     public void hideSystemIconArea(boolean animate) {
         animateHide(mSystemIconArea, animate);
         animateHide(mCenterClockLayout, animate);
+        if ((Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER,  0,
+                UserHandle.USER_CURRENT) == 2) ||
+			(Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER,  0,
+                UserHandle.USER_CURRENT) == 3)) {
         animateHide(mCarrierLabel,animate);
+        }
     }
+    
+
 
     public void showSystemIconArea(boolean animate) {
         animateShow(mSystemIconArea, animate);
         animateShow(mCenterClockLayout, animate);
+        if ((Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER,  0,
+                UserHandle.USER_CURRENT) == 2) ||
+			(Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER,  0,
+                UserHandle.USER_CURRENT) == 3)) {
         animateShow(mCarrierLabel,animate);
+        }
     }
 
     public void hideNotificationIconArea(boolean animate) {
