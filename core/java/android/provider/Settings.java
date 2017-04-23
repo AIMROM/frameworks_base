a140e863bfe99155bdbbfff80a6f6b2364cfaece
Anandnv <anand.nv1998@gmail.com>
2017-04-23 08:42:31 +0530

    FWD: FIX su icon hider --removed mismerged lines --changed key --Removed blourry part while disabling su(thx to RR)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3707,6 +3707,13 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
 
         /**
+          * whether to display SU icon in statusbar
+          * @hide
+          */
+        public static final String SHOW_SU_INDICATOR = "show_su_indicator";
+
+
+        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
