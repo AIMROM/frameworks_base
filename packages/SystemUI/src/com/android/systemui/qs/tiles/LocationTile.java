ed15db5dd9e5c7b7955dc5effdde769ef52e927f
Anandnv <anand.nv1998@gmail.com>
2017-04-28 20:17:03 +0530

    Update QS Location icon with accent colors signed-off-by Anand Ramachandran<anand.nv1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
@@ -103,7 +103,7 @@ public class LocationTile extends QSTileImpl<BooleanState> {
         state.value = locationEnabled;
         checkIfRestrictionEnforcedByAdminOnly(state, UserManager.DISALLOW_SHARE_LOCATION);
         if (locationEnabled) {
-            state.icon = mEnable;
+            state.icon =  ResourceIcon.get(R.drawable.ic_qs_location_high_accuracy);
             state.label = mContext.getString(R.string.quick_settings_location_label);
             state.contentDescription = mContext.getString(
                     R.string.accessibility_quick_settings_location_on);
