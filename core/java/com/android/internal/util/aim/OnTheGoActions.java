crdroidandroid/android_frameworks_base

On-The-Go Mode [1/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/util/crdroid/OnTheGoActions.java
@@ -1,0 +1,51 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.crdroid;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnTheGoActions {
+
+    public static final String ACTION_ONTHEGO_TOGGLE = "action_onthego_toggle";
+
+    public static void processAction(final Context context, final String action) {
+
+        if (action == null || action.isEmpty()) {
+            return;
+        }
+
+        if (ACTION_ONTHEGO_TOGGLE.equals(action)) {
+            actionOnTheGoToggle(context);
+        }
+
+    }
+
+    private static void actionOnTheGoToggle(final Context context) {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.crdroid.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        context.startService(startIntent);
+    }
+
+}

core/java/com/android/internal/util/crdroid/OnTheGoUtils.java
@@ -1,0 +1,106 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.crdroid;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.util.List;
+
+public class OnTheGoUtils {
+
+    private static final String TAG = "OnTheGoUtils";
+
+    /**
+     * Checks if a specific package is installed.
+     *
+     * @param context     The context to retrieve the package manager
+     * @param packageName The name of the package
+     * @return Whether the package is installed or not.
+     */
+    public static boolean isPackageInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            if (pm != null) {
+                List<ApplicationInfo> packages = pm.getInstalledApplications(0);
+                for (ApplicationInfo packageInfo : packages) {
+                    if (packageInfo.packageName.equals(packageName)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error: " + e.getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a specific service is running.
+     *
+     * @param context     The context to retrieve the activity manager
+     * @param serviceName The name of the service
+     * @return Whether the service is running or not
+     */
+    public static boolean isServiceRunning(Context context, String serviceName) {
+        ActivityManager activityManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> services = activityManager
+                .getRunningServices(Integer.MAX_VALUE);
+
+        if (services != null) {
+            for (ActivityManager.RunningServiceInfo info : services) {
+                if (info.service != null) {
+                    if (info.service.getClassName() != null && info.service.getClassName()
+                            .equalsIgnoreCase(serviceName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if system has a camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    /**
+     * Check if system has a front camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasFrontCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+    }
+}

packages/SystemUI/src/com/android/systemui/crdroid/onthego/OnTheGoDialog.java
@@ -1,0 +1,171 @@
+/*
+ * Copyright (C) 2014 The NamelessRom Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.crdroid.onthego;
+
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import com.android.internal.util.crdroid.OnTheGoUtils;
+import com.android.systemui.R;
+
+public class OnTheGoDialog extends Dialog {
+
+    protected final Context mContext;
+    protected final Handler mHandler = new Handler();
+
+    private final int mOnTheGoDialogLongTimeout;
+    private final int mOnTheGoDialogShortTimeout;
+
+    private final Runnable mDismissDialogRunnable = new Runnable() {
+        public void run() {
+            if (OnTheGoDialog.this.isShowing()) {
+                OnTheGoDialog.this.dismiss();
+            }
+        }
+    };
+
+    public OnTheGoDialog(Context ctx) {
+        super(ctx);
+        mContext = ctx;
+        final Resources r = mContext.getResources();
+        mOnTheGoDialogLongTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_long_timeout);
+        mOnTheGoDialogShortTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_short_timeout);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
+        window.getAttributes().privateFlags |=
+                WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.quick_settings_onthego_dialog);
+        setCanceledOnTouchOutside(true);
+
+        final ContentResolver resolver = mContext.getContentResolver();
+
+        final SeekBar mSlider = (SeekBar) findViewById(R.id.alpha_slider);
+        final float value = Settings.System.getFloat(resolver,
+                Settings.System.ON_THE_GO_ALPHA,
+                0.5f);
+        final int progress = ((int) (value * 100));
+        mSlider.setProgress(progress);
+        mSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sendAlphaBroadcast(String.valueOf(i + 10));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                removeAllOnTheGoDialogCallbacks();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+
+        if (!OnTheGoUtils.hasFrontCamera(getContext())) {
+            findViewById(R.id.onthego_category_1).setVisibility(View.GONE);
+        } else {
+            final Switch mServiceToggle = (Switch) findViewById(R.id.onthego_service_toggle);
+            final boolean restartService = Settings.System.getInt(resolver,
+                    Settings.System.ON_THE_GO_SERVICE_RESTART, 0) == 1;
+            mServiceToggle.setChecked(restartService);
+            mServiceToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putInt(resolver,
+                            Settings.System.ON_THE_GO_SERVICE_RESTART,
+                            (b ? 1 : 0));
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+
+            final Switch mCamSwitch = (Switch) findViewById(R.id.onthego_camera_toggle);
+            final boolean useFrontCam = (Settings.System.getInt(resolver,
+                    Settings.System.ON_THE_GO_CAMERA,
+                    0) == 1);
+            mCamSwitch.setChecked(useFrontCam);
+            mCamSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putInt(resolver,
+                            Settings.System.ON_THE_GO_CAMERA,
+                            (b ? 1 : 0));
+                    sendCameraBroadcast();
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dismissOnTheGoDialog(mOnTheGoDialogLongTimeout);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        removeAllOnTheGoDialogCallbacks();
+    }
+
+    private void dismissOnTheGoDialog(int timeout) {
+        removeAllOnTheGoDialogCallbacks();
+        mHandler.postDelayed(mDismissDialogRunnable, timeout);
+    }
+
+    private void removeAllOnTheGoDialogCallbacks() {
+        mHandler.removeCallbacks(mDismissDialogRunnable);
+    }
+
+    private void sendAlphaBroadcast(String i) {
+        final float value = (Float.parseFloat(i) / 100);
+        final Intent alphaBroadcast = new Intent();
+        alphaBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_ALPHA);
+        alphaBroadcast.putExtra(OnTheGoService.EXTRA_ALPHA, value);
+        mContext.sendBroadcast(alphaBroadcast);
+    }
+
+    private void sendCameraBroadcast() {
+        final Intent cameraBroadcast = new Intent();
+        cameraBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_CAMERA);
+        mContext.sendBroadcast(cameraBroadcast);
+    }
+
+}

services/core/java/com/android/server/policy/GlobalActions.java
@@ -103,6 +103,7 @@ import java.util.BitSet;
 import java.util.List;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
+import com.android.internal.util.crdroid.OnTheGoActions;
 
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
@@ -397,6 +398,35 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         onAirplaneModeChanged();
 
         mItems = new ArrayList<Action>();
+
+        // next: On-The-Go, if enabled
+	ContentResolver resolver = mContext.getContentResolver();
+        boolean showOnTheGo = Settings.System.getInt(
+                resolver, Settings.System.POWER_MENU_ONTHEGO_ENABLED, 0) == 1;
+        if (showOnTheGo) {
+            mItems.add(
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_onthego,
+                        R.string.global_action_onthego) {
+
+                        public void onPress() {
+                            OnTheGoActions.processAction(mContext,
+                                    OnTheGoActions.ACTION_ONTHEGO_TOGGLE);
+                        }
+
+                        public boolean onLongPress() {
+                            return false;
+                        }
+
+                        public boolean showDuringKeyguard() {
+                            return true;
+                        }
+
+                        public boolean showBeforeProvisioning() {
+                            return true;
+                        }
+                    }
+            );
+        }
 
         String[] actionsArray;
         if (mActions == null) {
@@ -844,6 +874,15 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 }
             }
         }
+    }
+
+    private void startOnTheGo() {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.crdroid.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        mContext.startService(startIntent);
     }
 
     /**

AIMROM/frameworks_base

Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

core/java/com/android/internal/util/aim/OnTheGoActions.java
@@ -1,0 +1,51 @@
+/*
+* <!--
+*    Copyright (C) 2017 AIMROM
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.aim;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnTheGoActions {
+
+    public static final String ACTION_ONTHEGO_TOGGLE = "action_onthego_toggle";
+
+    public static void processAction(final Context context, final String action) {
+
+        if (action == null || action.isEmpty()) {
+            return;
+        }
+
+        if (ACTION_ONTHEGO_TOGGLE.equals(action)) {
+            actionOnTheGoToggle(context);
+        }
+
+    }
+
+    private static void actionOnTheGoToggle(final Context context) {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.aim.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        context.startService(startIntent);
+    }
+
+}

core/java/com/android/internal/util/aim/OnTheGoUtils.java
@@ -1,0 +1,106 @@
+/*
+* <!--
+*    Copyright (C) 2017 AIMROM
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.aim;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.util.List;
+
+public class OnTheGoUtils {
+
+    private static final String TAG = "OnTheGoUtils";
+
+    /**
+     * Checks if a specific package is installed.
+     *
+     * @param context     The context to retrieve the package manager
+     * @param packageName The name of the package
+     * @return Whether the package is installed or not.
+     */
+    public static boolean isPackageInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            if (pm != null) {
+                List<ApplicationInfo> packages = pm.getInstalledApplications(0);
+                for (ApplicationInfo packageInfo : packages) {
+                    if (packageInfo.packageName.equals(packageName)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error: " + e.getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a specific service is running.
+     *
+     * @param context     The context to retrieve the activity manager
+     * @param serviceName The name of the service
+     * @return Whether the service is running or not
+     */
+    public static boolean isServiceRunning(Context context, String serviceName) {
+        ActivityManager activityManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> services = activityManager
+                .getRunningServices(Integer.MAX_VALUE);
+
+        if (services != null) {
+            for (ActivityManager.RunningServiceInfo info : services) {
+                if (info.service != null) {
+                    if (info.service.getClassName() != null && info.service.getClassName()
+                            .equalsIgnoreCase(serviceName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if system has a camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    /**
+     * Check if system has a front camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasFrontCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+    }
+}

packages/SystemUI/src/com/android/systemui/aim/onthego/OnTheGoDialog.java
@@ -1,0 +1,171 @@
+/*
+ * Copyright (C) 2017 AIMROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.aim.onthego;
+
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import com.android.internal.util.aim.OnTheGoUtils;
+import com.android.systemui.R;
+
+public class OnTheGoDialog extends Dialog {
+
+    protected final Context mContext;
+    protected final Handler mHandler = new Handler();
+
+    private final int mOnTheGoDialogLongTimeout;
+    private final int mOnTheGoDialogShortTimeout;
+
+    private final Runnable mDismissDialogRunnable = new Runnable() {
+        public void run() {
+            if (OnTheGoDialog.this.isShowing()) {
+                OnTheGoDialog.this.dismiss();
+            }
+        }
+    };
+
+    public OnTheGoDialog(Context ctx) {
+        super(ctx);
+        mContext = ctx;
+        final Resources r = mContext.getResources();
+        mOnTheGoDialogLongTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_long_timeout);
+        mOnTheGoDialogShortTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_short_timeout);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
+        window.getAttributes().privateFlags |=
+                WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.quick_settings_onthego_dialog);
+        setCanceledOnTouchOutside(true);
+
+        final ContentResolver resolver = mContext.getContentResolver();
+
+        final SeekBar mSlider = (SeekBar) findViewById(R.id.alpha_slider);
+        final float value = Settings.System.getFloat(resolver,
+                Settings.System.ON_THE_GO_ALPHA,
+                0.5f);
+        final int progress = ((int) (value * 100));
+        mSlider.setProgress(progress);
+        mSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sendAlphaBroadcast(String.valueOf(i + 10));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                removeAllOnTheGoDialogCallbacks();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+
+        if (!OnTheGoUtils.hasFrontCamera(getContext())) {
+            findViewById(R.id.onthego_category_1).setVisibility(View.GONE);
+        } else {
+            final Switch mServiceToggle = (Switch) findViewById(R.id.onthego_service_toggle);
+            final boolean restartService = Settings.System.getInt(resolver,
+                    Settings.System.ON_THE_GO_SERVICE_RESTART, 0) == 1;
+            mServiceToggle.setChecked(restartService);
+            mServiceToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putInt(resolver,
+                            Settings.System.ON_THE_GO_SERVICE_RESTART,
+                            (b ? 1 : 0));
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+
+            final Switch mCamSwitch = (Switch) findViewById(R.id.onthego_camera_toggle);
+            final boolean useFrontCam = (Settings.System.getInt(resolver,
+                    Settings.System.ON_THE_GO_CAMERA,
+                    0) == 1);
+            mCamSwitch.setChecked(useFrontCam);
+            mCamSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putInt(resolver,
+                            Settings.System.ON_THE_GO_CAMERA,
+                            (b ? 1 : 0));
+                    sendCameraBroadcast();
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dismissOnTheGoDialog(mOnTheGoDialogLongTimeout);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        removeAllOnTheGoDialogCallbacks();
+    }
+
+    private void dismissOnTheGoDialog(int timeout) {
+        removeAllOnTheGoDialogCallbacks();
+        mHandler.postDelayed(mDismissDialogRunnable, timeout);
+    }
+
+    private void removeAllOnTheGoDialogCallbacks() {
+        mHandler.removeCallbacks(mDismissDialogRunnable);
+    }
+
+    private void sendAlphaBroadcast(String i) {
+        final float value = (Float.parseFloat(i) / 100);
+        final Intent alphaBroadcast = new Intent();
+        alphaBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_ALPHA);
+        alphaBroadcast.putExtra(OnTheGoService.EXTRA_ALPHA, value);
+        mContext.sendBroadcast(alphaBroadcast);
+    }
+
+    private void sendCameraBroadcast() {
+        final Intent cameraBroadcast = new Intent();
+        cameraBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_CAMERA);
+        mContext.sendBroadcast(cameraBroadcast);
+    }
+
+}

services/core/java/com/android/server/policy/GlobalActions.java
@@ -102,6 +102,7 @@ import java.util.BitSet;
 import java.util.List;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
+import com.android.internal.util.aim.OnTheGoActions;
 
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
@@ -344,6 +345,35 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         onAirplaneModeChanged();
 
         mItems = new ArrayList<Action>();
+
+        // next: On-The-Go, if enabled
+	ContentResolver resolver = mContext.getContentResolver();
+        boolean showOnTheGo = Settings.System.getInt(
+                resolver, Settings.System.POWER_MENU_ONTHEGO_ENABLED, 0) == 1;
+        if (showOnTheGo) {
+            mItems.add(
+                new SinglePressAction(com.android.internal.R.drawable.ic_lock_onthego,
+                        R.string.global_action_onthego) {
+
+                        public void onPress() {
+                            OnTheGoActions.processAction(mContext,
+                                    OnTheGoActions.ACTION_ONTHEGO_TOGGLE);
+                        }
+
+                        public boolean onLongPress() {
+                            return false;
+                        }
+
+                        public boolean showDuringKeyguard() {
+                            return true;
+                        }
+
+                        public boolean showBeforeProvisioning() {
+                            return true;
+                        }
+                    }
+            );
+        }
 
         String[] actionsArray;
         if (mActions == null) {
@@ -779,6 +809,15 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 }
             }
         }
+    }
+
+    private void startOnTheGo() {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.aim.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        mContext.startService(startIntent);
     }
 
     /**