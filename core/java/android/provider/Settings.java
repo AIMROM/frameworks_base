4abe0628699743c39daa3a7da74842e9a01f6a93
xyyx <xyyx@mail.ru>
2017-08-28 17:17:28 +0800

    Mtp_Dirty_Hack: Use mtp by default in oreo too) Enabled by default
    Change-Id: Ifce53549b36f872bc302a847380f3a07236b7260
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4012,6 +4012,14 @@ public final class Settings {
            */
           public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
+	 /**
+          * Whether to use the MTP by default after connecting to PC
+          * @hide
+          */
+          public static final String MTP_DIRTY_HACK = "mtp_dirty_hack";
+          /** @hide */
+          public static final String MTP_DIRTY_HACK_SAVE = "mtp_dirty_hack_save";
+
 
 	/**
 	 * Setting to determine whether or not to show the battery percentage in the status bar.
