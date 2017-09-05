ea0bc8c1fc9d6c21248a913d7aa5a78403530bf6
ezio84 <brabus84@gmail.com>
2017-09-05 16:56:41 +0200

    Sysui tuner: add autohiding IME cursors as left/right nav buttons

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -221,6 +221,8 @@ public class NavigationBarView extends FrameLayout implements PluginListener<Nav
         mButtonDispatchers.put(R.id.ime_switcher, new ButtonDispatcher(R.id.ime_switcher));
         mButtonDispatchers.put(R.id.accessibility_button,
                 new ButtonDispatcher(R.id.accessibility_button));
+        mButtonDispatchers.put(R.id.kb_left, new ButtonDispatcher(R.id.kb_left));
+        mButtonDispatchers.put(R.id.kb_right, new ButtonDispatcher(R.id.kb_right));
     }
 
     public BarTransitions getBarTransitions() {
@@ -294,6 +296,14 @@ public class NavigationBarView extends FrameLayout implements PluginListener<Nav
 
     public ButtonDispatcher getAccessibilityButton() {
         return mButtonDispatchers.get(R.id.accessibility_button);
+    }
+
+    public ButtonDispatcher getKbLeftButton() {
+        return mButtonDispatchers.get(R.id.kb_left);
+    }
+
+    public ButtonDispatcher getKbRightButton() {
+        return mButtonDispatchers.get(R.id.kb_right);
     }
 
     public SparseArray<ButtonDispatcher> getButtonDispatchers() {
@@ -407,6 +417,8 @@ public class NavigationBarView extends FrameLayout implements PluginListener<Nav
                 : getBackIcon(mUseCarModeUi, mVertical);
 
         getBackButton().setImageDrawable(backIcon);
+        getKbLeftButton().setVisibility(backAlt ? View.VISIBLE : View.INVISIBLE);
+        getKbRightButton().setVisibility(backAlt ? View.VISIBLE : View.INVISIBLE);
 
         updateRecentsIcon();
 
