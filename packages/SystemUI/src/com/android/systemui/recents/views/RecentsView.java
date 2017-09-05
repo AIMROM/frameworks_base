3730379db61730cca3d8cdffe7f4d61970b9e2a4
Michael W <baddaemon87@gmail.com>
2017-09-05 13:15:42 +0800

    SystemUI: Recents: Allow closing empty view via tapping * Same as https://review.cyanogenmod.org/#/c/97718/ on new code base

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -79,6 +79,7 @@ import com.android.systemui.recents.events.activity.LaunchTaskEvent;
 import com.android.systemui.recents.events.activity.MultiWindowStateChangedEvent;
 import com.android.systemui.recents.events.activity.ShowEmptyViewEvent;
 import com.android.systemui.recents.events.activity.ShowStackActionButtonEvent;
+import com.android.systemui.recents.events.activity.ToggleRecentsEvent;
 import com.android.systemui.recents.events.component.ExpandPipEvent;
 import com.android.systemui.recents.events.ui.AllTaskViewsDismissedEvent;
 import com.android.systemui.recents.events.ui.DismissAllTaskViewsEvent;
@@ -425,6 +426,12 @@ public class RecentsView extends FrameLayout {
         if (RecentsDebugFlags.Static.EnableStackActionButton) {
             mStackActionButton.bringToFront();
         }
+        setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventBus.getDefault().send(new ToggleRecentsEvent());
+            }
+        });
     }
 
     /**
@@ -437,6 +444,7 @@ public class RecentsView extends FrameLayout {
         if (RecentsDebugFlags.Static.EnableStackActionButton) {
             mStackActionButton.bringToFront();
         }
+        setOnClickListener(null);
     }
 
     /**
@@ -642,7 +650,11 @@ public class RecentsView extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        return mTouchHandler.onTouchEvent(ev);
+        if (mTouchHandler.onTouchEvent(ev)) {
+            return true;
+        } else {
+            return super.onTouchEvent(ev);
+        }
     }
 
     @Override
