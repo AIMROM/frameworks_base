2877ec23e56891444fc66ed6678630976f5e6ae2
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    base: Disable dashboard suggestions and conditions [1/3]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5750,6 +5750,18 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to show dashboard conditions in Settings
+         * @hide
+         */
+        public static final String SETTINGS_SHOW_CONDITIONS = "settings_show_conditions";
+
+        /**
+         * Whether to show dashboard suggestions in Settings
+         * @hide
+         */
+        public static final String SETTINGS_SHOW_SUGGESTIONS = "settings_show_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
