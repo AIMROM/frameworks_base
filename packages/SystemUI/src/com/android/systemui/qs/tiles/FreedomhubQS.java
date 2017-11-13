289e6b0ffe08ddfab0f5e5efcab325b660b6fb83
klabit87 <keith.labit@gmail.com>
2017-11-12 19:45:06 -0600

    Fix FreedomHub QSTile derps

--- packages/SystemUI/src/com/android/systemui/qs/tiles/FreedomhubQS.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/FreedomhubQS.java
@@ -3,22 +3,28 @@ package com.android.systemui.qs.tiles;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ComponentName;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.Settings;
 import android.widget.Toast;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.R;
-import com.android.systemui.SysUIToast;
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.SysUIToast;
 import android.service.quicksettings.Tile;
-import com.android.systemui.qs.QSTileView;
+import com.android.systemui.qs.tileimpl.QSTileView;
 
 public class FreedomhubQS extends QSTileImpl<BooleanState> {
     private boolean mListening;
 
-    private static final Intent FREEDOMHUB = new Intent().setComponent(new ComponentName(
-            "com.android.settings", "com.android.settings.Settings$freedomhubSettingsActivity"));
+//    private static final Intent FREEDOMHUB = new Intent().setComponent(new ComponentName(
+//            "com.android.settings", "com.android.settings.Settings$freedomhubSettingsActivity"));
 
-    public FreedomhubQS(Host host) {
+    public FreedomhubQS(QSHost host) {
         super(host);
     }
 
@@ -49,23 +55,26 @@ public class FreedomhubQS extends QSTileImpl<BooleanState> {
         // Collapse the panels, so the user can see the toast.
         mHost.collapsePanels();
         SysUIToast.makeText(mContext, mContext.getString(
-                R.string.quick_freedomhub_toast),
+                R.string.quick_freedom_toast),
                 Toast.LENGTH_LONG).show();
     }
 
     @Override
     public CharSequence getTileLabel() {
-        return mContext.getString(R.string.quick_freedomhub_label);
+        return mContext.getString(R.string.quick_freedom_label);
     }
 
-    protected void startfreedomhub() {
-        mHost.startActivityDismissingKeyguard(FREEDOMHUB);
+    protected Intent startfreedomhub() {
+//        mHost.startActivityDismissingKeyguard(FREEDOMHUB);
+        return new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$freedomhubSettingsActivity"));
+
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
         state.icon = ResourceIcon.get(R.drawable.ic_qs_freedomhub);
-        state.label = mContext.getString(R.string.quick_freedomhub_label);
+        state.label = mContext.getString(R.string.quick_freedom_label);
     }
 
     @Override
