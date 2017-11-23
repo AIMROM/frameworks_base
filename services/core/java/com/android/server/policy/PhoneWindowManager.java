b412e192871944142424e007bb84f9d2d1c98f01
Paul Keith <javelinanddart@gmail.com>
2017-11-23 21:47:51 +0100

    fw/b: Return a KeyEvent instead of a boolean in KeyHandler
    * Allows handlers to modify the event before sending it off
      to another KeyHandler class, to handle things like rotation
    
    Change-Id: I481107e050f6323c5897260a5d241e64b4e031ac
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4336,7 +4336,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (DEBUG_INPUT) {
                     Log.d(TAG, "Dispatching key event " + event + " to handler " + handler);
                 }
-                if (handler.handleKeyEvent(event)) {
+                event = handler.handleKeyEvent(event);
+                if (event == null) {
                     return true;
                 }
             } catch (Exception e) {
