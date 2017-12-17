04751402b911e0dec9570dd3a0472e96114597a4
Anand Ramachandran <anand.nv1998@gmail.com>
2017-12-17 09:41:36 -0500

    Get ready to Welcome DUI :) squashed revert of following Navbar Additions
    Revert "Add NavBar toggle [1/2]"
    
    This reverts commit 0bbf3bb.
    
    Change-Id: Ifdad475f4b79c6b5706e0ae81f5e5dbf20a0de78
    
    Revert "Add navbar double tap to sleep [1/2]"
    
    This reverts commit 400ab13.
    
    Change-Id: I04d1b57244e5fbdc8323b97685067da655c919c1
    
    Revert "Run NavBarTuner external"
    
    This reverts commit 1c68b4c.
    
    Change-Id: I750c5a2530a09484d2e5ec2893d2cf1aec8b6ecb
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -851,10 +851,4 @@ public class NavigationBarView extends FrameLayout implements PluginListener<Nav
         updateRecentsIcon();
     });
 
-    public void setDoubleTapToSleep() {
-        boolean isDoubleTapEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.DOUBLE_TAP_SLEEP_NAVBAR, 1, UserHandle.USER_CURRENT) == 1;
-        ((NavigationBarFrame) getRootView()).setDoubleTapToSleep(isDoubleTapEnabled);
-    }
-
 }
