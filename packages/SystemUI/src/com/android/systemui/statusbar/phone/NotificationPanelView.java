edd07f0f8972bc50a3698715882594a763c34784
ezio84 <brabus84@gmail.com>
2018-01-19 15:30:10 +0100

    Ambient music: let hide clock and show track info in center screen [1/2]
    when a new track is played, allow to show track infos only,
    without the clock and notifications icons.
    In this case, also move track info to the screen center.
    
    This doesn't affect normal system ambient notifications, they'll keep
    clock and other stuff.
    Change-Id: I4b249464f17a1742f8926a48d63a82b11e3bc075

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2715,6 +2715,10 @@ public class NotificationPanelView extends PanelView implements
         mKeyguardStatusView.setPulsing(pulsing);
     }
 
+    public void setForcedMediaPulse(int reason) {
+        mKeyguardStatusView.setForcedMediaPulse(reason);
+    }
+
     public void setAmbientIndicationBottomPadding(int ambientIndicationBottomPadding) {
         if (mAmbientIndicationBottomPadding != ambientIndicationBottomPadding) {
             mAmbientIndicationBottomPadding = ambientIndicationBottomPadding;
