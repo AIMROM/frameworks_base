013c5401f63a098666ab87680bba08e7bf0a92fe
ezio84 <brabus84@gmail.com>
2018-01-20 10:40:48 +0100

    Ambient music ticker: add a full disable mode [1/2]
    and improve functions names and settings part
    
    Change-Id: I045602fd34d63b93eb611bc8e07672254a898ef8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2713,8 +2713,8 @@ public class NotificationPanelView extends PanelView implements
         mKeyguardStatusView.setPulsing(pulsing);
     }
 
-    public void setForcedMediaPulse(int reason) {
-        mKeyguardStatusView.setForcedMediaPulse(reason);
+    public void setCleanLayout(int reason) {
+        mKeyguardStatusView.setCleanLayout(reason);
     }
 
     public void setAmbientIndicationBottomPadding(int ambientIndicationBottomPadding) {
