7b36a668b9438ab3355dbb54e16a3edce2111c58
ezio84 <brabus84@gmail.com>
2018-01-24 12:16:44 +0100

    DUI Pulse: add Automatic albumart base color mode [1/3]
    the dui part will also be sure that pulse color has
    an acceptable contrast against white and black navbar
    
    Also expose media notification colors values for future improvements
    
    Change-Id: I1bd6b70f2f5ad1f0e6decdac311b1c8e51c9149d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -627,6 +627,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected PorterDuffXfermode mSrcOverXferMode =
             new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER);
 
+    private Entry mEntryToRefresh;
     private NotificationManager mNoMan;
     private String[] mNavMediaArrowsExcludeList;
     private MediaSessionManager mMediaSessionManager;
@@ -681,6 +682,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 }
             }
             if (mNavigationBar != null) {
+                // pulse colors already set by titckTrackInfo
                 mNavigationBar.setMediaPlaying(true);
             }
         } else {
@@ -707,6 +709,10 @@ public class StatusBar extends SystemUI implements DemoMode,
                 if (isAmbientContainerAvailable()) {
                     ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
                 }
+                // NotificationInflater calls async MediaNotificationProcessoron to create notification
+                // colors and when finished will trigger AsyncInflationFinished for all registered callbacks
+                // like StatusBar. From there we'll send updated colors to Pulse
+                mEntryToRefresh = entry;
                 break;
             }
         }
@@ -1939,6 +1945,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateNotificationShade();
         }
         entry.row.setLowPriorityStateUpdated(false);
+
+        if (mEntryToRefresh == entry) {
+            if (mNavigationBar != null) {
+                Notification n = entry.notification.getNotification();
+                int[] colors = {n.backgroundColor, n.foregroundColor,
+                        n.primaryTextColor, n.secondaryTextColor};
+                mNavigationBar.setPulseColors(n.isColorizedMedia(), colors);
+            }
+        }
     }
 
     private boolean shouldSuppressFullScreenIntent(String key) {
