753996f61fbf93b8ff54f3acbc87c90cf1e7a24f
ezio84 <brabus84@gmail.com>
2018-03-07 12:31:44 +0100

    Stock Recents: use Slim Recents icons management [1/2]
    Change-Id: Id5fd224cc21977edc0eed4c27283164324f94145

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -217,7 +217,7 @@ import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
 import com.android.systemui.recents.events.activity.AppTransitionFinishedEvent;
 import com.android.systemui.recents.events.activity.UndockingTaskEvent;
-import com.android.systemui.recents.misc.IconPackHelper;
+import com.android.systemui.slimrecent.icons.IconsHandler;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.settings.BrightnessController;
 import com.android.systemui.slimrecent.RecentController;
@@ -6554,7 +6554,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             setQsRowsColumns();
             setUseLessBoringHeadsUp();
             updateTickerAnimation();
-            updateRecentsIconPack();
             updateRecentsMode();
             updateTheme();
             setForceAmbient();
@@ -6623,7 +6622,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (!slimRecents) {
             String currentIconPack = Settings.System.getStringForUser(mContext.getContentResolver(),
                 Settings.System.RECENTS_ICON_PACK, mCurrentUserId);
-            IconPackHelper.getInstance(mContext).updatePrefs(currentIconPack);
+            IconsHandler.getInstance(mContext).updatePrefs(currentIconPack);
             mRecents.resetIconCache();
         }
     }
@@ -6643,6 +6642,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mSlimRecents = null;
             }
         }
+        IconsHandler.getInstance(mContext).resetIconNormalizer();
+        updateRecentsIconPack();
     }
 
     private void rebuildRecentsScreen() {
