d66a469b3052e82a96e0757fa377018e7e1ed500
Kshitij Gupta <kshitijgm@gmail.com>
2018-05-15 11:36:13 +0000

    QS: Set Colors [1/2]
    Change-Id: I575d39ab3758fba79084a8657fca5b7a56557364
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4159,6 +4159,12 @@ public final class Settings {
          public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
 	 /**
+          * Whether to change the color of the qs panel
+          * @hide
+          */
+         public static final String QS_PANEL_BG_COLOR = "qs_panel_bg_color";
+
+	 /**
 	  * Whether to use the MTP by default after connecting to PC
           * @hide
           */
