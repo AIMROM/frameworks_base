4ea8d138b7228d5f3f67462415faf1e05db34a83
Kshitij Gupta <kshitijgm@gmail.com>
2018-05-26 05:50:21 +0000

    base: Add setting to toggle icon tint in settings [1/3]
    Change-Id: Ifa79910c2b1b555186025b30acda46ad7db920b1
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3203,7 +3203,18 @@ public class StatusBar extends SystemUI implements DemoMode,
         return themeInfo != null && themeInfo.isEnabled();
     }
 
-    @Nullable
+   public boolean isUsingIconTint() {
+         OverlayInfo themeInfo = null;
+         try {
+             themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.settingsicontint",
+                     mCurrentUserId);
+         } catch (RemoteException e) {
+             e.printStackTrace();
+         }
+         return themeInfo != null && themeInfo.isEnabled();
+     }
+
+   @Nullable
     public View getAmbientIndicationContainer() {
         return mAmbientIndicationContainer;
     }
@@ -5391,6 +5402,18 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
 
         }
+
+	int userSettingsIconTint = Settings.System.getIntForUser(mContext.getContentResolver(),
+                     Settings.System.SETTINGS_ICON_TINT, 0, mCurrentUserId);
+         boolean setSettingsIconTint = userSettingsIconTint == 1;
+         if(setSettingsIconTint != isUsingIconTint()) {
+              try {
+                 mOverlayManager.setEnabled("com.android.system.theme.settingsicontint",
+                         setSettingsIconTint, mCurrentUserId);
+             } catch (RemoteException e) {
+                 Log.w(TAG, "Can't change icon tint", e);
+             }
+         }
     }
 
     private void updateDozingState() {
@@ -6626,6 +6649,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
                     false, this, UserHandle.USER_ALL);
+	   mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                     Settings.System.SETTINGS_ICON_TINT),
+                     false, this, UserHandle.USER_ALL);
 
             update();
         }
@@ -6675,6 +6701,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE))) {
                 setStatusBarWindowViewOptions();
+	   } else if (uri.equals(Settings.System.getUriFor(Settings.System.SETTINGS_ICON_TINT))) {
+                 updateTheme();
             }
         }
 
