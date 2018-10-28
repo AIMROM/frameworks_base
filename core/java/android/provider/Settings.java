822fdb9f9b92d5d6930ece26484358d6e8acd10d
Pranav Vashi <neobuddy89@gmail.com>
2018-10-28 20:56:46 +0530

    Ability to toggle VoLTE icon in statusbar [1/2]
    * Complete rewrite for PIE.
    * Adapted for Android 10.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I8ce485a285a0cdd95d0f2d281f9003c9a937d12a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4719,6 +4719,12 @@ public final class Settings {
         public static final String FOD_ICON_WALLPAPER_COLOR = "fod_icon_wallpaper_color";
 
         /**
+         * Whether to display VoLTE icon when VoLTE present
+         * @hide
+         */
+        public static final String SHOW_VOLTE_ICON = "show_volte_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
