8b196c103f84425e3ff7a4657a993af09a61e377
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-25 11:10:02 +0530

    Disable Notifications vibration [1/2]
    Change-Id: I3e821155a588cba9285e97d3d36bced92f9b988f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4963,6 +4963,12 @@ public final class Settings {
         public static final String LOCKSCREEN_CHARGING_ANIMATION = "lockscreen_charging_animation";
 
         /**
+         * Whether to vibrate on notifications
+         * @hide
+         */
+        public static final String VIBRATE_ON_NOTIFICATIONS = "vibrate_on_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
