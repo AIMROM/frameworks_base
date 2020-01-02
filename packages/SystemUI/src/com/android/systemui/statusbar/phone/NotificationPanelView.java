af98b41b9e1bd6eeccbc8e630458eb1ac9bb79c1
Alex <du.alexcruz@gmail.com>
2020-01-02 16:48:02 -0500

    Introduce lockscreen clock/date styles [1/2]
    Inspired by OnePlus and their recent options to switch lockscreen
    clock styles on the OP6, I took it upon myself to do the same with our ROM.
    
    This isn't as fancy as OnePlus' because is just using a list preference instead
    of adding in all the eye candy but it gets the job done!
    
    Below is a run down of what took place...
    
    - Manipulated the clockview format for both 12 and 24 hour to achieved the 'sammy' style
    - Made the sammy style optional and created a switch case for all 3.
    - Set padding, singeline and other layout changes programmatically to minimize the jank
    - Reworked/cleaned up some existing code (Huge thanks to ezio84)
    - Created bold sammy by using html formatting, based off the sammy one
    - Created bold digital by using html formatting, based off the digital one
    
    - Left out the analog clocks this time around. Too much of a hassle.
    
    For right now there's five clock styles:
    
    Clock styles---
    - Hidden
    - Default
    - Digital (bold)
    - Sammy
    - Sammy (bold)
    
    Change-Id: I2cfa59f6347fe779a3cf6d9b5239a71cea098666

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -3553,6 +3553,10 @@ public class NotificationPanelView extends PanelView implements
         }
     }
 
+    public void updateKeyguardStatusSettings() {
+        mKeyguardStatusView.updateAll();
+    }
+
     public void setStatusAccessibilityImportance(int mode) {
         mKeyguardStatusView.setImportantForAccessibility(mode);
     }
