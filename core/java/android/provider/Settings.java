ea27b046f61074fddf0a7dea96fe615301319b7d
00day0 <therandomuser11@gmail.com>
2020-01-16 10:50:01 +0200

    SystemUI: add FOD icon picker and wallpaper based color option [1/2]
    * Co-authored-by: Stallix <joey@evolution-x.org> Co-authored-by:
     rohanpurohit <purohit.rohan@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4705,6 +4705,18 @@ public final class Settings {
         private static final Validator LOCKSCREEN_MEDIA_BLUR_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * FOD icon picker
+         * @hide
+         */
+        public static final String FOD_ICON = "fod_icon";
+
+        /**
+         * FOD use wallpaper color
+         * @hide
+         */
+        public static final String FOD_ICON_WALLPAPER_COLOR = "fod_icon_wallpaper_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
