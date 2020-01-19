158d3b1688918f299c9fa17cdebf8f7f3baa3754
El Dainosor <eldainosor@gmail.com>
2020-01-19 07:44:18 +0100

    base: Let's shrink file headers bigger than the device size
    Custom headers and new cameras with bigger resolution sizes aren't a good match.
    This might put a stop to those who put a 4K image as their header and complains about SysUI struggling :)
    
    Change-Id: I4a9ade0008f532d84160f44c57efdaa3e1fdeb30
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- packages/SystemUI/src/com/android/systemui/aim/header/FileHeaderProvider.java
+++ packages/SystemUI/src/com/android/systemui/aim/header/FileHeaderProvider.java
@@ -26,10 +26,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.view.WindowManager;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -115,12 +117,60 @@ public class FileHeaderProvider implements
         if (file.exists()) {
             if (DEBUG) Log.i(TAG, "Load header image");
             final Bitmap image = BitmapFactory.decodeFile(file.getAbsolutePath());
-            mImage = new BitmapDrawable(mContext.getResources(), image);
+            mImage = new BitmapDrawable(mContext.getResources(), resizeMaxDeviceSize(mContext, image));
         }
     }
 
     @Override
     public Drawable getCurrent(final Calendar now) {
         return mImage;
+    }
+
+    public static Bitmap resizeMaxDeviceSize(Context context, Drawable image) {
+        Bitmap i2b = ((BitmapDrawable) image).getBitmap();
+        return resizeMaxDeviceSize(context, i2b);
+    }
+
+    public static Bitmap resizeMaxDeviceSize(Context context, Bitmap image) {
+        Bitmap imageToBitmap;
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = context.getSystemService(WindowManager.class);
+        wm.getDefaultDisplay().getRealMetrics(metrics);
+        int maxHeight = metrics.heightPixels;
+        int maxWidth = metrics.widthPixels;
+        try {
+            imageToBitmap = RGB565toARGB888(image);
+            if (maxHeight > 0 && maxWidth > 0) {
+                int width = imageToBitmap.getWidth();
+                int height = imageToBitmap.getHeight();
+                float ratioBitmap = (float) width / (float) height;
+                float ratioMax = (float) maxWidth / (float) maxHeight;
+
+                int finalWidth = maxWidth;
+                int finalHeight = maxHeight;
+                if (ratioMax > ratioBitmap) {
+                    finalWidth = (int) ((float)maxHeight * ratioBitmap);
+                } else {
+                    finalHeight = (int) ((float)maxWidth / ratioBitmap);
+                }
+                imageToBitmap = Bitmap.createScaledBitmap(imageToBitmap, finalWidth, finalHeight, true);
+                return imageToBitmap;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    private static Bitmap RGB565toARGB888(Bitmap img) throws Exception {
+        int numPixels = img.getWidth() * img.getHeight();
+        int[] pixels = new int[numPixels];
+        //Get JPEG pixels.  Each int is the color values for one pixel.
+        img.getPixels(pixels, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+        //Create a Bitmap of the appropriate format.
+        Bitmap result = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+        //Set RGB pixels.
+        result.setPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+        return result;
     }
 }
