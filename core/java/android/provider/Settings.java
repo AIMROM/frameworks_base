bf13c23c60aa20476391d54f2fb30782d3137b75
00day0 <therandomuser11@gmail.com>
2020-01-28 00:07:58 +0100

    FODCircleView: Recognizing fingerprint animation [1/2]
    Change-Id: I6fc7e9634f2075b9224edf799135e65eb01e4e11

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4719,6 +4719,12 @@ public final class Settings {
         public static final String FOD_ICON_WALLPAPER_COLOR = "fod_icon_wallpaper_color";
 
         /**
+         * FOD recognizing animation
+         * @hide
+         */
+        public static final String FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
+
+        /**
          * Whether to display VoLTE icon when VoLTE present
          * @hide
          */
