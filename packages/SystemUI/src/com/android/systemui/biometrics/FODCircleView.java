183b0e84781f9139ca45a7060ce8b9c526f66cfb
00day0 <therandomuser11@gmail.com>
2020-03-10 21:34:41 +0530

    FODCircleView: add empty icon
    Idk, someone asked me to do it xD
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Change-Id: I969c78f6c2c6522fced8945872a6df9b2e5fc003

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -119,7 +119,8 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         R.drawable.fod_icon_rainbow_horn,
         R.drawable.fod_icon_shooky,
         R.drawable.fod_icon_spiral_blue,
-        R.drawable.fod_icon_sun_metro
+        R.drawable.fod_icon_sun_metro,
+        0
     };
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
