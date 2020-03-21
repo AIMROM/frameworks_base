LineageOS/android_frameworks_base

[BACKPORT]NetworkManagement : Add ability to restrict app vpn usage
Cherry-picked from https://review.lineageos.org/#/c/LineageOS/android_frameworks_base/+/232796/

VPN restriction independent from WiFi and mobile data.

Change-Id: I18728542e4a22733f4e096dc429386e61f2a3a06

core/java/android/net/NetworkPolicyManager.java
@@ -52,6 +52,8 @@ public class NetworkPolicyManager {
     public static final int POLICY_REJECT_ON_WIFI = 0x8000;
     /** Reject application network traffic on cellular network **/
     public static final int POLICY_REJECT_ON_DATA = 0x10000;
+    /** Reject application network traffic on virtual private network */
+    public static final int POLICY_REJECT_ON_VPN = 0x12000;
 
     /*
      * Rules defining whether an uid has access to a network given its type (metered / non-metered).

AIMROM/frameworks_base

Signed-off-by: Albert I <kras@raphielgang.org>

core/java/android/net/NetworkPolicyManager.java
@@ -52,6 +52,8 @@ public class NetworkPolicyManager {
     public static final int POLICY_REJECT_ON_WIFI = 0x8000;
     /** Reject application network traffic on cellular network **/
     public static final int POLICY_REJECT_ON_DATA = 0x10000;
+    /** Reject application network traffic on virtual private network */
+    public static final int POLICY_REJECT_ON_VPN = 0x02000;
 
     /*
      * Rules defining whether an uid has access to a network given its type (metered / non-metered).