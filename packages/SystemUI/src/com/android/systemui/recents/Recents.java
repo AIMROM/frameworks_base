omnirom/android_frameworks_base_old

[1/2] base: OmniSwitch integration
do it on a lower level this time so we catch all
calls in one place

Change-Id: Ic603d0dde9fcf1f266b1bff2eb89dae5345168b7

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -46,6 +46,7 @@ import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.omni.OmniSwitchConstants;
 import com.android.systemui.Dependency;
 import com.android.systemui.EventLogConstants;
 import com.android.systemui.EventLogTags;
@@ -259,6 +260,11 @@ public class Recents extends SystemUI
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            OmniSwitchConstants.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         IOverviewProxy overviewProxy = mOverviewProxyService.getProxy();
         if (overviewProxy != null) {
             try {
@@ -304,6 +310,11 @@ public class Recents extends SystemUI
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            OmniSwitchConstants.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         IOverviewProxy overviewProxy = mOverviewProxyService.getProxy();
         if (overviewProxy != null) {
             try {
@@ -342,6 +353,11 @@ public class Recents extends SystemUI
         // Ensure the device has been provisioned before allowing the user to interact with
         // recents
         if (!isUserSetup()) {
+            return;
+        }
+
+        if (isOmniSwitchRecents()) {
+            OmniSwitchConstants.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
             return;
         }
 
@@ -404,6 +420,10 @@ public class Recents extends SystemUI
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            OmniSwitchConstants.preloadOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
         if (mOverviewProxyService.getProxy() != null) {
             // TODO: Proxy to Launcher
             return;
@@ -436,7 +456,9 @@ public class Recents extends SystemUI
         if (!isUserSetup()) {
             return;
         }
-
+        if (isOmniSwitchRecents()) {
+            return;
+        }
         if (mOverviewProxyService.getProxy() != null) {
             // TODO: Proxy to Launcher
             return;
@@ -890,5 +912,11 @@ public class Recents extends SystemUI
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         pw.println("Recents");
         pw.println("  currentUserId=" + SystemServicesProxy.getInstance(mContext).getCurrentUser());
+    }
+
+    private boolean isOmniSwitchRecents() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_NAVIGATION_BAR_RECENTS, 0,
+                    UserHandle.USER_CURRENT) == 1;
     }
 }

AIMROM/frameworks_base

Change-Id: I53d83bb8e288f78bec558086ae33cfedb19c1e71
Signed-off-by: Ravi Pingat <pingatravi@gmail.com>

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -19,9 +19,12 @@ package com.android.systemui.recents;
 import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.graphics.Rect;
+import android.os.UserHandle;
 import android.provider.Settings;
 
 import com.android.systemui.Dependency;
+import com.android.internal.util.aim.Utils;
+
 import com.android.systemui.R;
 import com.android.systemui.SystemUI;
 import com.android.systemui.statusbar.CommandQueue;
@@ -87,6 +90,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks, TunerSe
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            Utils.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         mImpl.showRecentApps(triggeredFromAltTab);
     }
 
@@ -95,6 +103,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks, TunerSe
         // Ensure the device has been provisioned before allowing the user to interact with
         // recents
         if (!isUserSetup()) {
+            return;
+        }
+
+        if (isOmniSwitchRecents()) {
+            Utils.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
             return;
         }
 
@@ -109,6 +122,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks, TunerSe
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            Utils.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         mImpl.toggleRecentApps();
     }
 
@@ -117,6 +135,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks, TunerSe
         // Ensure the device has been provisioned before allowing the user to interact with
         // recents
         if (!isUserSetup()) {
+            return;
+        }
+
+        if (isOmniSwitchRecents()) {
+            Utils.preloadOmniSwitchRecents(mContext, UserHandle.CURRENT);
             return;
         }
 
@@ -130,7 +153,9 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks, TunerSe
         if (!isUserSetup()) {
             return;
         }
-
+        if (isOmniSwitchRecents()) {
+            return;
+        }
         mImpl.cancelPreloadRecentApps();
     }
 
@@ -213,4 +238,9 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks, TunerSe
         mImpl.dump(pw);
     }
 
+    private boolean isOmniSwitchRecents() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_NAVIGATION_BAR_RECENTS, 0,
+                    UserHandle.USER_CURRENT) == 1;
+    }
 }