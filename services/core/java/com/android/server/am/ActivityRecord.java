53b80dfffcd3cebb23eebfa79a3d05a3d0d3a3b7
Louis Chang <louischang@google.com>
2022-08-02 03:33:39 +0000

    [BACKPORT] Do not send new Intent to non-exported activity when navigateUpTo
    The new Intent was delivered to a non-exported activity while
    
    Bug: 238605611
    Test: atest StartActivityTests
    Change-Id: I854dd825bfd9a2c08851980d480d1f3a177af6cf
    Merged-In: I854dd825bfd9a2c08851980d480d1f3a177af6cf
    (cherry picked from commit b9a934064598aa655fab4ce75c8eab6165409670)
    Merged-In: I854dd825bfd9a2c08851980d480d1f3a177af6cf
    (cherry picked from commit 6ed145548d3d8edb788d0bf157aca0525355f472)
    Signed-off-by: Albert I <kras@raphielgang.org>

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -1534,6 +1534,10 @@ final class ActivityRecord {
         return info.applicationInfo.uid;
     }
 
+    int getPid() {
+        return app != null ? app.getPid() : 0;
+    }
+
     @Override
     public String toString() {
         if (stringName != null) {
